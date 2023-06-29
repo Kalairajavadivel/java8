776d4c20e2a70d893ad395e15a40308923f3b21a
kalaiselvi <kalairajavadivel@gmail.com>
2023-06-29 12:32:48 +0530

    initial commit

---
+++ src/com/kalai/jenkins/FindOddOrEvenWithJenkins.java
@@ -0,0 +1,17 @@
+package com.kalai.jenkins;
+
+public class FindOddOrEvenWithJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number = 39;
+
+		if (number % 2 == 0) {
+			System.out.println("Even number" + " " + number);
+		} else {
+			System.out.println("Odd number" + " " + number);
+		}
+	}
+
+}
