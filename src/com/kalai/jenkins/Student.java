88908742def9284c7deb024a170e3eb4d94ca143
kalaiselvi <kalairajavadivel@gmail.com>
2023-06-29 12:38:27 +0530

    second commit

---
+++ src/com/kalai/jenkins/Student.java
@@ -0,0 +1,25 @@
+package com.kalai.jenkins;
+
+public class Student {
+
+	int id;
+	String name;
+
+	Student(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	void display() {
+		System.out.println(id + " " + name);
+	}
+
+	public static void main(String[] args) {
+
+		Student s1 = new Student(109, "kalai");
+		Student s2 = new Student(110, "Nindhuja");
+
+		s1.display();
+		s2.display();
+	}
+}
